5db6999f8ec6ebbe52d8e6e4a85cf65b364543f9
Oleksii <alexmkh89@gmail.com>
2020-08-16 20:10:01 +0300

    add     public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class

---
+++ MenuMaker.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using MenuMaker.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MenuMaker.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public void Create(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public TEntity FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetEntities()
+        {
+            var result = _dbSet.AsNoTracking().ToList();
+            return result;
+        }
+
+        public IEnumerable<TEntity> GetEntities(Func<TEntity, bool> func)
+        {
+            var result = _dbSet.AsNoTracking().Where(func).ToList();
+            return result;
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
