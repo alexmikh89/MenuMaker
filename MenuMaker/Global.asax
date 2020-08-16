9a6d36faf8e975f2cd66a91e282dc4787069d50f
Oleksii <alexmkh89@gmail.com>
2020-08-16 10:38:13 +0300

    Add project files.

---
+++ MenuMaker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MenuMaker.MvcApplication" Language="C#" %>
---
+++ MenuMaker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MenuMaker.Startup))]
+namespace MenuMaker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
