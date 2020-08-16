ca8d5a4f9f8f3e61eea280a83d8e6da30a69d4d4
Oleksii <alexmkh89@gmail.com>
2020-08-16 19:45:11 +0300

    add     public interface IRepository<TEntity> where TEntity : class

---
+++ MenuMaker.Data/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MenuMaker.Data.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity entity);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> GetEntities();
+        IEnumerable<TEntity> GetEntities(Func<TEntity, bool> func);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
