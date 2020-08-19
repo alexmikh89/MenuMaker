01d2e5d6d3744e6d59730bb0a759fdd93f6ae362
Oleksii <alexmkh89@gmail.com>
2020-08-19 20:45:05 +0300

    add     public interface IManager<TEntityModel> in BL

---
+++ MenuMaker.Business/Interfaces/IManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MenuMaker.Business.Interfaces
+{
+    public interface IManager<TEntityModel> where TEntityModel : class
+    {
+        void Create(TEntityModel entity);
+        TEntityModel Read(int? id);
+        void Update(TEntityModel entity);
+        void Delete();
+        IEnumerable<TEntityModel> GetAll();
+    }
+}
