cc0539df58b26e4e68fe5521244abb22b58297cd
Oleksii <alexmkh89@gmail.com>
2020-08-19 19:14:57 +0300

    add     public class RecipeToIngridient to DAL

---
+++ MenuMaker.Data/Models/RecipeToIngridient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuMaker.Data.Models
+{
+    public class RecipeToIngridient
+    {
+        public int Id { get; set; }
+        public Recipe Recipe { get; set; }
+        public Ingridient Ingridient { get; set; }
+        public double Amount { get; set; }
+    }
+}
