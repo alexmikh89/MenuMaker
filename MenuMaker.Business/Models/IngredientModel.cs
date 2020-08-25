216e1ee05033b3aad2f39231c7b74815735427af
Oleksii <alexmkh89@gmail.com>
2020-08-25 19:57:00 +0300

    add IngredientModel to BL

--- MenuMaker.Business/Models/IngredientModel.cs
+++ MenuMaker.Business/Models/IngredientModel.cs
@@ -1,8 +1,11 @@
-﻿namespace MenuMaker.Business.Models
+﻿using System.Collections.Generic;
+
+namespace MenuMaker.Business.Models
 {
     public class IngredientModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public virtual ICollection<RecipeIngredientsModel> RecipeIngredientsModels { get; set; }
     }
 }
