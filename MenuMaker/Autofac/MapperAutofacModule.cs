7dbddb25eefd553240082f5f47e47ce9154c50f7
Oleksii <alexmkh89@gmail.com>
2020-08-26 20:12:07 +0300

    add mappings in MapperAutofacModule

--- MenuMaker/Autofac/MapperAutofacModule.cs
+++ MenuMaker/Autofac/MapperAutofacModule.cs
@@ -1,7 +1,9 @@
 ﻿using Autofac;
 using AutoMapper;
 using MenuMaker.Business.Models;
+using MenuMaker.Data.Models;
 using MenuMaker.Models;
+using System;
 
 namespace MenuMaker.Autofac
 {
@@ -11,10 +13,23 @@ namespace MenuMaker.Autofac
         {
             builder.Register(context => new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<IngredientViewModel, IngredientModel>().ReverseMap();
-                cfg.CreateMap<RecipeViewModel, RecipeModel>().ReverseMap();
-                cfg.CreateMap<RecipeIngredientsViewModel, RecipeIngredientsModel>().ReverseMap();
-            })).AsSelf().SingleInstance();
+                cfg.CreateMap<Recipe, RecipeModel>().ReverseMap();
+                cfg.CreateMap<Func<Recipe, bool>, Func<RecipeModel, bool>>().ReverseMap();
+                cfg.CreateMap<RecipeModel, RecipeViewModel>().ReverseMap();
+                cfg.CreateMap<Func<RecipeModel, bool>, Func<RecipeViewModel, bool>>().ReverseMap();
+
+                cfg.CreateMap<Ingredient, IngredientModel>().ReverseMap();
+                cfg.CreateMap<Func<Ingredient, bool>, Func<IngredientModel, bool>>().ReverseMap();
+                cfg.CreateMap<IngredientModel, IngredientViewModel>().ReverseMap();
+                cfg.CreateMap<Func<IngredientModel, bool>, Func<IngredientViewModel, bool>>().ReverseMap();
+
+                cfg.CreateMap<RecipeIngredients, RecipeIngredientsModel>().ReverseMap();
+                cfg.CreateMap<Func<RecipeIngredients, bool>, Func<RecipeIngredientsModel, bool>>().ReverseMap();
+                cfg.CreateMap<RecipeIngredientsModel, RecipeIngredientsViewModel>().ReverseMap();
+                cfg.CreateMap<Func<RecipeIngredientsModel, bool>, Func<RecipeIngredientsViewModel, bool>>().ReverseMap();
+            }))
+                .AsSelf()
+                .SingleInstance();
 
             builder.Register(i =>
             {
