d7235722fa5cc3934dc45aaf7d19857cd3140ee6
Oleksii <alexmkh89@gmail.com>
2020-09-01 19:08:35 +0300

    add abstract class BaseManager to BL

--- MenuMaker.Business/Managers/EntityManager.cs
+++ MenuMaker.Business/Managers/EntityManager.cs
@@ -1,60 +1,14 @@
 ﻿using AutoMapper;
-using MenuMaker.Business.Interfaces;
 using MenuMaker.Data.Interfaces;
-using System;
-using System.Collections.Generic;
 
 namespace MenuMaker.Business.Managers
 {
-    public class EntityManager<Entity, EntityModel> : IEntityManager<Entity, EntityModel>
-        where Entity : class
-        where EntityModel : class
+    public class EntityManager<Entity, EntityModel> : BaseManager<Entity, EntityModel>
+    where Entity : class
+    where EntityModel : class
     {
-        private readonly IRepository<Entity> _repository;
-        private readonly IMapper _mapper;
-
-        public EntityManager(IMapper mapper, IRepository<Entity> repository)
+        public EntityManager(IMapper mapper, IRepository<Entity> repository) : base(mapper, repository)
         {
-            _repository = repository;
-            _mapper = mapper;
-        }
-
-        public void Create(EntityModel entityModel)
-        {
-            var dbEntity = _mapper.Map<Entity>(entityModel);
-            _repository.Create(dbEntity);
-        }
-
-        public EntityModel FindById(int? id)
-        {
-            var dbEntity = _repository.FindById(id);
-            return _mapper.Map<EntityModel>(dbEntity);
-        }
-
-        public IEnumerable<EntityModel> GetAll()
-        {
-            var listOfDbEntities = _repository.GetAll();
-            var listOfEntityModels = _mapper.Map<IEnumerable<EntityModel>>(listOfDbEntities);
-            return listOfEntityModels;
-        }
-
-        public IEnumerable<EntityModel> GetAll(Func<EntityModel, bool> blFunc)
-        {
-            var dbFunc = _mapper.Map<Func<Entity, bool>>(blFunc);
-            var listOfDbEntities = _repository.GetAll(dbFunc);
-            var listOfEntityModels = _mapper.Map<IEnumerable<EntityModel>>(listOfDbEntities);
-            return listOfEntityModels;
-        }
-
-        public void Remove(int? id)
-        {
-            _repository.Remove(id);
-        }
-
-        public void Update(EntityModel entityModel)
-        {
-            var dbEntityToUpdate = _mapper.Map<Entity>(entityModel);
-            _repository.Update(dbEntityToUpdate);
         }
     }
 }
--- MenuMaker/Controllers/RecipeViewModelsController.cs
+++ MenuMaker/Controllers/RecipeViewModelsController.cs
@@ -70,7 +70,7 @@ namespace MenuMaker.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        //[ValidateAntiForgeryToken]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(RecipeIngredientPostVM recipeIngredientPostVM)
         {
             if (ModelState.IsValid)
