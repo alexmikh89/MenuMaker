702d6bd8f41f89c11fdcfa9160a3d952bd730031
Oleksii <alexmkh89@gmail.com>
2020-09-06 19:31:32 +0300

    add MenuRecipePostViewModel in PL

---
+++ MenuMaker/Models/MenuRecipePostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MenuMaker.Models
+{
+    public class MenuRecipePostViewModel
+    {
+        public int[] RecipeId { get; set; }
+        public int[] DayId { get; set; }
+    }
+}
